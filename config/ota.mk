fced805ae3357b72cf8dda85cfc12de099dea925
Shubham Singh <coolsks94@gmail.com>
2019-09-25 14:08:26 +0000

    Vendor: Allow shiping both gapps and non-gapps builds

--- config/ota.mk
+++ config/ota.mk
@@ -1,6 +1,12 @@
 ifeq ($(CUSTOM_BUILD_TYPE), OFFICIAL)
 
+ifeq ($(TARGET_SHIPS_SEPERATE_GAPPS_BUILD), true)
+ifeq ($(WITH_GAPPS), true)
+CUSTOM_OTA_VERSION_CODE := ten_gapps
+else
 CUSTOM_OTA_VERSION_CODE := ten
+endif
+endif
 
 CUSTOM_PROPERTIES += \
     org.cerberus.ota.version_code=$(CUSTOM_OTA_VERSION_CODE) \
